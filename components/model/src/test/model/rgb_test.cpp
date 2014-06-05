e1ac4d8ff89b99b1448f6f9e267bc444fe71c357
Early Ehlinger <early.ehlinger@clearcapital.com>
2014-06-04 23:23:42 -0500

    - Updated rgb_test to throw exceptions rather than assert().

--- components/model/src/test/model/rgb_test.cpp
+++ components/model/src/test/model/rgb_test.cpp
@@ -1,67 +1,61 @@
 #include <model/rgb.hpp>
 
-#include <cassert>
-#include <iostream>
+#include <stdexcept>
 
 void test_rgb_default_ctor() 
 {
-  std::cout << "test " << __func__ << std::endl;
   phase3d::model::rgb<double> rgbdbl;
 
-  assert(rgbdbl.red() == 0.0);
-  assert(rgbdbl.green() == 0.0);
-  assert(rgbdbl.blue() == 0.0);
+  if (rgbdbl.red() != 0.0) throw std::runtime_error( "expected rgbdbl.red() == 0.0");
+  if (rgbdbl.green() != 0.0) throw std::runtime_error( "expected rgbdbl.green() == 0.0");
+  if (rgbdbl.blue() != 0.0) throw std::runtime_error( "expected rgbdbl.blue() == 0.0" );
 }
 
 void test_rgb_color_ctor()
 {
-  std::cout << "test " << __func__ << std::endl;
   phase3d::model::rgb<double> rgbdbl(0.25,0.5,1);
 
-  assert(rgbdbl.red()==0.25);
-  assert(rgbdbl.green()==0.5);
-  assert(rgbdbl.blue()==1.0);
+  if(rgbdbl.red()!=0.25) throw std::runtime_error( "expected rgbdbl.red() == 0.25");
+  if(rgbdbl.green()!=0.5) throw std::runtime_error( "expected green==0.5" );
+  if(rgbdbl.blue()!=1.0) throw std::runtime_error( "expected blue==1.0" );
 }
 
-void test_setters()
+void test_rgb_setters()
 {
-  std::cout << "test " << __func__ << std::endl;
   phase3d::model::rgb<double> rgbdbl;
 
   rgbdbl.red(0.25).green(0.5).blue(1.0);
   
-  assert(rgbdbl.red()==0.25);
-  assert(rgbdbl.green()==0.5);
-  assert(rgbdbl.blue()==1.0);
+  if(rgbdbl.red()!=0.25) throw std::runtime_error("expected red==0.25");
+  if(rgbdbl.green()!=0.5) throw std::runtime_error("expected green==0.5");
+  if(rgbdbl.blue()!=1.0) throw std::runtime_error("expected blue==1.0");
 }
 
-void test_unary_negative()
+void test_rgb_unary_negative()
 {
-  std::cout << "test " << __func__ << std::endl;
   phase3d::model::rgb<double> rgbdbl(0.25,0.5,1);
 
   rgbdbl = -rgbdbl;
 
-  assert(rgbdbl.red() == -0.25);
-  assert(rgbdbl.green() == -0.5);
-  assert(rgbdbl.blue() == -1.0);
+  if(rgbdbl.red() != -0.25) throw std::runtime_error("expected red== -0.25");
+  if(rgbdbl.green() != -0.5) throw std::runtime_error("expected green== -0.5");
+  if(rgbdbl.blue() != -1.0) throw std::runtime_error("expected blue== -1.0");
 }
 
-void test_scalar_multiplication()
+void test_rgb_scalar_multiplication()
 {
-  std::cout << "test " << __func__ << std::endl;
   phase3d::model::rgb<double> rgbdbl(0.25,0.5,1);
 
   rgbdbl = 2.0 * rgbdbl;
 
-  assert(rgbdbl.red() == 0.5);
-  assert(rgbdbl.green() == 1.0);
-  assert(rgbdbl.blue() == 2.0);
+  if(rgbdbl.red() != 0.5) throw std::runtime_error("expected red==0.5");
+  if(rgbdbl.green() != 1.0) throw std::runtime_error("expected green==1.0");
+  if(rgbdbl.blue() != 2.0) throw std::runtime_error("expected blue==2.0");
 
   rgbdbl = rgbdbl * 2.0;
 
-  assert(rgbdbl.red() == 1.0);
-  assert(rgbdbl.green() == 2.0);
-  assert(rgbdbl.blue() == 4.0);
+  if(rgbdbl.red() != 1.0) throw std::runtime_error("expected red==1.0");
+  if(rgbdbl.green() != 2.0) throw std::runtime_error("expected green==2.0");
+  if(rgbdbl.blue() != 4.0) throw std::runtime_error("expected blue==4.0");
 }
 
